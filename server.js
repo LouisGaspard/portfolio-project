d25e3141f2b07d72d64f4fc795e66987b305631e
Louis Gaspard <louis.gaspard@epitech.eu>
2018-06-21 13:42:01 +0100

    remove database

--- server.js
+++ server.js
@@ -4,23 +4,6 @@ var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 var port = process.env.PORT || 8080;
 
-const { Client } = require('pg');
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-    client.end();
-});
-
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
